34ae05c95cf7909a0621bdf9cd65379e9b340fa9
Randy Fields <rfields@rexairllc.com>
2024-11-08 09:22:13 -0500

    simple command test

--- src/main.py
+++ src/main.py
@@ -8,48 +8,71 @@ from rexserial import serialPolling
 import asyncio
 
 
-
 class MainGUI:
     def __init__(self, mainWindow):
         self.assets_dir = Path(__file__).parent.parent / "assets"
         self.logo_dir = os.path.join(self.assets_dir, "Rexair-LLC.png")
-        
+
         mainWindow.title("Rexair Automation Controller")
         self.mainWindowWidth = mainWindow.winfo_screenwidth()
         self.mainWindowHeight = mainWindow.winfo_screenheight()
         mainWindow.geometry(f"{self.mainWindowWidth}x{self.mainWindowHeight}+0+0")
 
-        self.scanResultsPane = tk.Frame(mainWindow, width=int(self.mainWindowWidth/4), height=int(self.mainWindowHeight*0.9))
+        self.scanResultsPane = tk.Frame(
+            mainWindow,
+            width=int(self.mainWindowWidth / 4),
+            height=int(self.mainWindowHeight * 0.9),
+        )
         self.scanResultsPane.pack_propagate(False)
-        self.scanResultsPane.pack(side='left', anchor="w")
+        self.scanResultsPane.pack(side="left", anchor="w")
         self.canvases = []
         for i in range(7):
-            canvas = tk.Canvas(self.scanResultsPane, relief='raised', borderwidth=2, width=int(self.mainWindowWidth/4), height=int(self.mainWindowHeight*0.075), bg="white")
+            canvas = tk.Canvas(
+                self.scanResultsPane,
+                relief="raised",
+                borderwidth=2,
+                width=int(self.mainWindowWidth / 4),
+                height=int(self.mainWindowHeight * 0.075),
+                bg="white",
+            )
             canvas.pack()
             self.canvases.append(canvas)
-        
-        self.controlPanel = tk.Frame(mainWindow, width=int(self.mainWindowWidth/4), height=int(self.mainWindowHeight*0.9))
+
+        self.controlPanel = tk.Frame(
+            mainWindow,
+            width=int(self.mainWindowWidth / 4),
+            height=int(self.mainWindowHeight * 0.9),
+        )
         self.controlPanel.pack_propagate(False)
-        self.controlPanel.pack(side='left', anchor="w")
+        self.controlPanel.pack(side="left", anchor="w")
 
-        self.scanButton = tk.Button(self.controlPanel, text='Scan', command=self.drawScan)
+        self.scanButton = tk.Button(
+            self.controlPanel, text="Scan", command=self.drawScan
+        )
         self.scanButton.config(height=3, width=10)
-        self.scanButton.pack(side='top')
-        self.clearButton = tk.Button(self.controlPanel, text='Clear', command=self.clearScan)
+        self.scanButton.pack(side="top")
+        self.clearButton = tk.Button(
+            self.controlPanel, text="Clear", command=self.clearScan
+        )
         self.clearButton.config(height=3, width=10)
-        self.clearButton.pack(side='top')
+        self.clearButton.pack(side="top")
 
-        self.logoPane = tk.Frame(mainWindow, width=int(self.mainWindowWidth/4), height=int(self.mainWindowHeight*0.9))
+        self.logoPane = tk.Frame(
+            mainWindow,
+            width=int(self.mainWindowWidth / 4),
+            height=int(self.mainWindowHeight * 0.9),
+        )
         self.logoPane.pack_propagate(False)
-        self.logoPane.pack(side='right', anchor="e")
+        self.logoPane.pack(side="right", anchor="e")
         self.image = Image.open(self.logo_dir)
-        self.image = self.image.resize((int(self.mainWindowWidth/10), int(self.mainWindowHeight/10)))
+        self.image = self.image.resize(
+            (int(self.mainWindowWidth / 10), int(self.mainWindowHeight / 10))
+        )
         self.image = ImageTk.PhotoImage(self.image)
-        self.bg_label = tk.Label(self.logoPane, image = self.image)
+        self.bg_label = tk.Label(self.logoPane, image=self.image)
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
         self.bg_label.image = self.image
         self.bg_label.pack(side="bottom")
-
 
         sv_ttk.set_theme("light")
 
@@ -70,7 +93,7 @@ class MainGUI:
         self.cellheight = 10
         for column in range(4):
             for row in range(4):
-                x1 = column*self.cellwidth
+                x1 = column * self.cellwidth
                 y1 = row * self.cellheight
                 x2 = x1 + self.cellwidth
                 y2 = y1 + self.cellheight
@@ -83,11 +106,17 @@ class MainGUI:
                 else:
                     color = "white"
 
-                self.rect[row,column] = self.canvases[index].create_rectangle(x1,y1,x2,y2, fill=color, tags="rect")
-                self.oval[row,column] = self.canvases[index].create_oval(x1+2,y1+2,x2-2,y2-2, fill="gray", tags="oval")
+                self.rect[row, column] = self.canvases[index].create_rectangle(
+                    x1, y1, x2, y2, fill=color, tags="rect"
+                )
+                self.oval[row, column] = self.canvases[index].create_oval(
+                    x1 + 2, y1 + 2, x2 - 2, y2 - 2, fill="gray", tags="oval"
+                )
         xpos = self.canvases[index].winfo_width() - 10
         ypos = 50
-        self.canvases[index].create_text(xpos, ypos, anchor='e', text=f"Station {index+1}", font=("Arial", 16))
+        self.canvases[index].create_text(
+            xpos, ypos, anchor="e", text=f"Station {index+1}", font=("Arial", 16)
+        )
 
     def drawScan(self):
         for val in range(7):
@@ -97,17 +126,15 @@ class MainGUI:
         for i in range(7):
             self.canvases[i].delete("all")
 
-
     def saveValues(self, rootVal, GUIVal):
         self.root = rootVal
         self.GUI = GUIVal
 
- 
 
 async def main():
     root = tk.Tk()
     GUI = MainGUI(root)
-    comPort = serialPolling('/dev/ttyS1', 115200, 1)
+    comPort = serialPolling("/dev/ttyS1", 115200, 1)
     GUI.saveValues(root, GUI)
 
     while True:
@@ -119,10 +146,10 @@ async def main():
         newval = comPort.PktDecode(value)
         print(newval)
         await comPort.pollWriteController(value)
-        result = await comPort.pollReadController()
-        print(result)
+        # result = await comPort.pollReadController()
+        # print(result)
         await asyncio.sleep(0.2)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     asyncio.run(main())
