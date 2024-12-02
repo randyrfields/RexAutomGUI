12ccff6905f4e0d397dd0d7acbf541c771321930
Randy Fields <rfields@rexairllc.com>
2024-12-02 08:06:46 -0500

    polling test

--- src/rexserial.py
+++ src/rexserial.py
@@ -92,5 +92,5 @@ class serialPolling:
         await self.pollWriteController(requestStatusPkt)
         time.sleep(0.1)
         response = await self.pollReadController()
-        print("response=%02x\n")
+        print("response=%2d\n" % response)
         return response
