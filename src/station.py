9068973fd33a4885b6178ff022279ed79f9886b3
Randy Fields <rfields@rexairllc.com>
2024-12-04 10:52:02 -0500

    correct nodestatus array

--- src/station.py
+++ src/station.py
@@ -23,13 +23,14 @@ class Station:
         # Create list for status storage
         for i in range(1, 8):
             row = []
-            for j in range(4):
+            for j in range(8):
                 row.append(0)
-            self.nodeStatus.insert(i, row)
+            self.nodeStatus.append(row)
 
     async def performScan(self):
         for x in range(1, 8):
             result = await self.serial.Poll(x, SysControlCommands.GETSTATUS)
             # result = bytes([0xA7, 0x08, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00])
-            self.nodeStatus.insert(x, list(result[2:8]))
+            self.nodeStatus[x].insert(0, list(result[2:8]))
+            # self.nodeStatus.insert(x, list(result[2:8]))
             # print(self.nodeStatus)
