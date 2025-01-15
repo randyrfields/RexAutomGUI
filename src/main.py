bf5e283bfb717550eb561886aa68ac520ca20a8d
Randy Fields <rfields@rexairllc.com>
2025-01-15 14:03:19 -0500

    initialize array

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ class GUI(customtkinter.CTk):
         self.station_button = []
 
         self.TOFData = [[0] * 16 for x in range(8)]
-        self.stationType = [[0] * 8]
+        self.stationType = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
         # configure window
         self.os = platform.system()
