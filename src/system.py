b6a776b1f4b0cf57e65f401ff2a493c5ef440082
Randy Fields <rfields@rexairllc.com>
2025-01-22 09:13:25 -0500

    diagnostic

--- src/system.py
+++ src/system.py
@@ -91,7 +91,7 @@ class SystemController:
                 else:
                     self.gui.clearLiveStation()
             else:
-                await data = self.scanDiags()
+                data = await self.scanDiags()
                 print(chr(27) + "[2J")
                 print("Node:  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15")
                 # print("       0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0")
