af3cdf0d82ce716d6670a7dea11074576f8faa52
Randy Fields <rfields@rexairllc.com>
2025-01-22 10:39:29 -0500

    diagnostic

--- src/system.py
+++ src/system.py
@@ -95,14 +95,9 @@ class SystemController:
             else:
                 data = await self.scanDiags()
                 print(chr(27) + "[2J")
-                print(data)
-                print(Count)
-                Count += 1
                 print("Node:  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15")
-                # print("       0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0")
                 print("     ", end=" ")
-                for i in range(16):
-                    # data.append(i)
+                for i in range(3, 19):
                     print(f"{data[i]:2d}", end=" ")
-                print("-----------------")
+                print(" ")
                 time.sleep(1)
