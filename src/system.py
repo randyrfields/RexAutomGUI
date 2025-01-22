c46f1f4c412d64ae1ef72147f6049e2d52d14b41
Randy Fields <rfields@rexairllc.com>
2025-01-22 09:54:44 -0500

    diagnostic

--- src/system.py
+++ src/system.py
@@ -94,6 +94,7 @@ class SystemController:
             else:
                 data = await self.scanDiags()
                 print(chr(27) + "[2J")
+                print(data)
                 print("Node:  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15")
                 # print("       0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0")
                 print("     ", end=" ")
