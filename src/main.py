1d4093f3ed4c72bcbf33b8c336007bfee33e1333
Randy Fields <randyrfields@gmail.com>
2025-12-23 14:38:59 -0500

    add cal full button

--- src/main.py
+++ src/main.py
@@ -326,8 +326,18 @@ class GUI(customtkinter.CTk):
             # command=lambda: self.saveStationInfo(number),
         )
 
-        CalEmptyButton.pack(pady=5, side= "bottom")
+        CalFullButton = customtkinter.CTkButton(
+            self.stationSettings,
+            text="Cal Full",
+            height=40,
+            width=250,
+            font=("Consolas", 25, "bold"),
+            # command=lambda: self.saveStationInfo(number),
+        )
+
         saveButton.pack(pady=5, side="bottom")
+        CalFullButton.pack(pady=5, side= "bottom")
+        CalEmptyButton.pack(pady=5, side= "bottom")
 
     def saveStationInfo(self, number):
         x = number
